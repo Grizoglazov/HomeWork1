6efc3ea6ed8453276b06f238c7f6770e6fa270c6
Andrey_Grizoglazov <greeza@yandex.ru>
2023-05-21 22:44:22 +0300

    Create task2

--- Task2/Program.cs
+++ Task2/Program.cs
@@ -3,3 +3,28 @@
 // 44 5 78 -> 78
 // 22 3 9 -> 22
 
+Console.WriteLine("Давай найдем максимальное число!");
+Console.WriteLine("Введите последовательно три числа");
+string input_1 = Console.ReadLine();
+int number_1 = Convert.ToInt32(input_1);
+string input_2 = Console.ReadLine();
+int number_2 = Convert.ToInt32(input_2);
+string input_3 = Console.ReadLine();
+int number_3 = Convert.ToInt32(input_3);
+if (number_1>number_2)
+    {
+        if (number_1>number_3)
+        {
+            Console.WriteLine("Максимальное число " + number_1);
+        }
+        else
+            Console.WriteLine("Максимальное число " + number_3);
+    }
+else if (number_2>number_3)
+    {
+        Console.WriteLine("Максимальное число " + number_2);
+    }
+else 
+    Console.WriteLine("Максимальное число " + number_3);
+    
+   
